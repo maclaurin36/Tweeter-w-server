e9d7f8b39745ab090faa105fd96e9cffc0fdbc55
JesseClark36 <jesse.clark@awardco.com>
2022-10-31 11:08:47 -0600

    Got stuff working

--- app/src/main/java/edu/byu/cs/tweeter/client/model/net/ServerFacade.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/net/ServerFacade.java
@@ -16,7 +16,7 @@ public class ServerFacade {
 
     // TODO: Set this to the invoke URL of your API. Find it by going to your API in AWS, clicking
     //  on stages in the right-side menu, and clicking on the stage you deployed your API to.
-    private static final String SERVER_URL = "Insert your API invoke URL here";
+    private static final String SERVER_URL = "https://d9q8fvxn5h.execute-api.us-west-1.amazonaws.com/Production";
 
     private final ClientCommunicator clientCommunicator = new ClientCommunicator(SERVER_URL);
 
