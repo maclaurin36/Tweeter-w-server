17be4af9bdc0cfca9f883884576f8546d9929671
JesseClark36 <jesse.clark@awardco.com>
2022-11-01 19:05:31 -0600

    Finished with the responses

--- shared/src/main/java/edu/byu/cs/tweeter/model/net/response/CountResponse.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/net/response/CountResponse.java
@@ -1,10 +1,15 @@
 package edu.byu.cs.tweeter.model.net.response;
 
-public class CountResponse {
+public class CountResponse extends Response {
     private int count;
 
     public CountResponse(int count) {
+        super(true);
         this.count = count;
+    }
+
+    public CountResponse(String message) {
+        super(false, message);
     }
 
     public int getCount() {
--- shared/src/main/java/edu/byu/cs/tweeter/model/net/response/IsFollowerResponse.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/net/response/IsFollowerResponse.java
@@ -1,4 +1,18 @@
 package edu.byu.cs.tweeter.model.net.response;
 
-public class IsFollowerResponse {
+public class IsFollowerResponse extends Response {
+    private boolean isFollower;
+
+    public IsFollowerResponse(boolean isFollower) {
+        super(true);
+        this.isFollower = isFollower;
+    }
+
+    public IsFollowerResponse(String message) {
+        super(false, message);
+    }
+
+    public boolean getIsFollower() {
+        return this.isFollower;
+    }
 }
